f74d006b82b36f48b5b0c4ccda832137b7bc6d78
DevUTP <C29396@utp.edu.pe>
2025-04-16 15:40:10 -0500

    Agregando funcionalidad js

---
+++ funciones.js
@@ -0,0 +1,16 @@
+function Operaciones(a,b,Operacion){
+    switch (Operacion) {
+        case valor1:
+          a+b;
+          break;
+        case 2:
+            a-b;
+            break;
+        case 3:
+            a*b;
+            break;
+        case 3:
+            a/b;
+            break;
+      }
+}
